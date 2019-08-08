a6d17bdfc57bf505b8c2bdef0ec16577cec5c7e8
lj72808up <lj72808up@163.com>
2019-08-09 01:46:24 +0800

    codes

--- 剑指offer/swordtoffer/src/main/java/other/MaxProductAfterCutting_14.java
+++ 剑指offer/swordtoffer/src/main/java/other/MaxProductAfterCutting_14.java
@@ -8,7 +8,34 @@ package other;
  */
 public class MaxProductAfterCutting_14 {
     public int maxProductAfterCutting(int length) {
-
+        if(length<=1)
+            return 0;
+        if(length==2)
+            return 1;
+        if(length==3)
+            return 2;
+        int[] res = new int[length+1];
+        res[0] = 1;
+        res[1] = 1;
+        res[2] = 2;
+        res[3] = 3;
+        for (int i = 4; i <=length ; i++) {
+            int product = 0;
+            for (int j = 0; j <=i/2 ; j++) {
+                int tmp = res[j]*res[i-j];
+                if (tmp>product)
+                    product = tmp;
+            }
+            res[i] = product;
+        }
+        return res[length];
     }
 }
 //https://www.acwing.com/problem/content/description/24/
+
+
+/**
+ * 思路:
+ *   动态规划的思想: f(n)=max(f(k)*f(n-k)) , (n>3)
+ *   用长度为n+1的一维数组存储每个长度的最优解
+ * */
