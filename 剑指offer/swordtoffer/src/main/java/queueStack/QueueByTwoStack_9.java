eca34ad596ebd58dbc9a4ae34b86b4b25df33e6e
lj72808up <lj72808up@163.com>
2019-08-14 02:02:30 +0800

    codes

---
+++ 剑指offer/swordtoffer/src/main/java/queueStack/QueueByTwoStack_9.java
@@ -0,0 +1,21 @@
+package queueStack;
+
+import java.util.Stack;
+
+/**
+ * 题目描述
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&tqId=11158&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class QueueByTwoStack_9 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+
+    }
+
+    public int pop() {
+
+    }
+}
