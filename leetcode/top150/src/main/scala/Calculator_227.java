e4ba142f57c027ba762926718064c2655753d97d
lj72808up <lj72808up@163.com>
2019-08-16 13:55:56 +0800

    codes

---
+++ leetcode/top150/src/main/scala/Calculator_227.java
@@ -0,0 +1,18 @@
+/**
+ * 题目描述:
+ * 实现一个基本的计算器来计算一个简单的字符串表达式的值。
+ * 字符串表达式仅包含非负整数，+， - ，*，/ 四种运算符和空格  。 整数除法仅保留整数部分。
+ *
+ * 示例 1:
+ *   输入: "3+2*2"
+ *   输出: 7
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/basic-calculator-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Calculator_227 {
+    public int calculate(String s) {
+        return 0;
+    }
+}
